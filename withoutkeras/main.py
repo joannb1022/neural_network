20f7a5be385c0c7e3adaf310409d178dc8dfff4c
bszlacht <73062284+bszlacht@users.noreply.github.com>
2021-04-08 21:24:04 +0200

    Add files via upload

--- withoutkeras/main.py
+++ withoutkeras/main.py
@@ -2,15 +2,60 @@ import tensorflow as tf
 from data import Data
 from tensorflow_model import my_model
 import numpy as np
+import matplotlib.pyplot as plt
+import window
+
 
 def one_hot_encode(vec):
+    n = len(vec)
+    out = np.zeros((n, 4))
+    for i in range(n):
+        out[i, vec[i]] = 1
 
-  n = len(vec)
-  out = np.zeros((n, 4))
-  for i in range(n):
-    out[i, vec[i]] = 1
+    return out
 
-  return out
+
+def loss_function(y_pred, y_true):
+    return tf.nn.softmax_cross_entropy_with_logits(labels=tf.stop_gradient(y_true), logits=y_pred)
+
+
+def get_next_batch(batch_size, i, images, labels):
+    x = images[i:i + batch_size]
+    y = labels[i:i + batch_size]
+    # self.i = (self.i + batch_size) % len(self.training_images)
+    return x, y
+
+
+def train_step(model, x_input, y_true, epoch):
+    epoch_accuracy = None
+    epoch_loss_avg = None
+
+    with tf.GradientTape() as tape:
+        # Get the predictions
+        preds = model.run(x_input)
+
+        # Calc the loss
+        current_loss = loss_function(preds, y_true)
+
+        # Get the gradients
+        grads = tape.gradient(current_loss, model.trainable_variables())
+
+        # Update the weights
+        optimizer.apply_gradients(zip(grads, model.trainable_variables()))
+
+        if epoch % 100 == 0:
+            y_pred = model.run(test_images)
+            matches = tf.equal(tf.math.argmax(y_pred, 1), tf.math.argmax(test_labels, 1))
+
+            epoch_accuracy = tf.reduce_mean(tf.cast(matches, tf.float32))
+            epoch_loss_avg = tf.reduce_mean(current_loss)
+
+            print("--- On epoch {} ---".format(epoch))
+            tf.print("Accuracy: ", epoch_accuracy, "| Loss: ", epoch_loss_avg)
+            print("\n")
+
+        return epoch_accuracy, epoch_loss_avg
+
 
 data = Data()
 train_images, train_labels = data.train_data_with_labels
@@ -22,75 +67,27 @@ train_labels = one_hot_encode(train_labels)
 test_images = test_images / 255.0
 test_labels = one_hot_encode(test_labels)
 
-
-
 model = my_model()
-
-def loss_function(y_pred,y_true):
-
-    return tf.nn.softmax_cross_entropy_with_logits(labels=tf.stop_gradient(y_true),logits=y_pred)
-
-
-def get_next_batch(batch_size, i, images, labels):
-  x = images[i:i+batch_size]
-  y = labels[i:i+batch_size]
-  #self.i = (self.i + batch_size) % len(self.training_images)
-  return x, y
 
 optimizer = tf.optimizers.Adam(learning_rate=0.001)
 
-def train_step( model, x_input , y_true, epoch):
-
-  epoch_accuracy = None
-  epoch_loss_avg = None
-
-  with tf.GradientTape() as tape:
-
-    # Get the predictions
-    preds = model.run(x_input)
-
-    # Calc the loss
-    current_loss = loss_function(preds, y_true)
-
-    # Get the gradients
-    grads = tape.gradient(current_loss, model.trainable_variables())
-
-    # Update the weights
-    optimizer.apply_gradients(zip(grads, model.trainable_variables()))
-
-    if epoch%100 == 0:
-
-      y_pred = model.run(test_images)
-      matches  = tf.equal(tf.math.argmax(y_pred,1), tf.math.argmax(test_labels,1))
-
-      epoch_accuracy = tf.reduce_mean(tf.cast(matches,tf.float32))
-      epoch_loss_avg = tf.reduce_mean(current_loss)
-
-      print("--- On epoch {} ---".format(epoch))
-      tf.print("Accuracy: ", epoch_accuracy, "| Loss: ",epoch_loss_avg)
-      print("\n")
-
-    return epoch_accuracy,epoch_loss_avg
-
-
-
-num_epochs = 5000
-batch_size = 100
+num_epochs = 0
+batch_size = 32
 
 train_loss_results = []
 train_accuracy_results = []
 
 for epoch in range(num_epochs):
 
-  # Get next batch
-  batch_x, batch_y = get_next_batch( batch_size, epoch, train_images, train_labels)
+    # Get next batch
+    batch_x, batch_y = get_next_batch(batch_size, epoch, train_images, train_labels)
 
-  # Train the model
-  epoch_accuracy, epoch_loss_avg = train_step(model, batch_x, batch_y, epoch)
+    # Train the model
+    epoch_accuracy, epoch_loss_avg = train_step(model, batch_x, batch_y, epoch)
 
-  if(epoch_loss_avg is not None):
-    train_loss_results.append(epoch_loss_avg)
-    train_accuracy_results.append(epoch_accuracy)
+    if epoch_loss_avg is not None:
+        train_loss_results.append(epoch_loss_avg)
+        train_accuracy_results.append(epoch_accuracy)
 
 plt.plot(train_loss_results)
 plt.title('Loss')
@@ -98,3 +95,6 @@ plt.show()
 plt.title('Accuracy')
 plt.plot(train_accuracy_results)
 plt.show()
+
+okno = window.InputWindow(3, model)
+okno.show()
--- withoutkeras/window.py
+++ withoutkeras/window.py
@@ -1,42 +1,39 @@
 import PySimpleGUI as sg
 from keras import Model
-from preparedata import create_data, create_training_set, show_random_shapes
+from preparedata import create_data, show_random_shapes, create_data_with_labels
+from withoutkeras.data import Data
+
 
 class InputWindow:
 
-    def __init__(self, default_number):
+    def __init__(self, default_number, model):
         self.image_number = default_number
-        self.layout = [[sg.Text('Shape Classification', size=(40, 1), justification='center', font='Helvetica 20')],                       [sg.Text("Pick number of shapes :")],
-                       [sg.Spin([i for i in range(1, 20, 1)], initial_value=4, key="-NUM-")],
-                       [sg.Text("Or pick image:"), sg.FileBrowse()],
-                       [sg.Checkbox('Show plots', key="-PLOT-")],
-                       [sg.Button('Train', size=(10, 1),  font='Helvetica 14')],
-                       [sg.Button('Recognize', size=(10, 1), font='Helvetica 14')],
+        self.layout = [[sg.Text('Shape Classification', size=(40, 1), justification='center', font='Helvetica 20')],
+                       [sg.Text("There are 4 shapes:")],
+                       [sg.Text("circle, square, triangle, star")],
+                       [sg.Button('Start', size=(10, 1), font='Helvetica 14')],
                        [sg.Button('Exit', size=(10, 1), font='Helvetica 14')]]
         self.interrupted = False
         self.plots = False
+        self.model = model
 
     def show(self):
         window = sg.Window('Main Window', self.layout, location=(800, 400))
-
 
         while True:
             event, values = window.read()
             if event == 'Exit' or event == sg.WIN_CLOSED:
                 self.interrupted = True
                 break
-            if event == "Train":
-                model = Model()
-                model.train()
-            if event == "Recognize":
-                self.image_number = values["-NUM-"]
-                if model.model == None:
-                    print("nieee")
-                    return 0
-                model.predict(self.image_number)
-                self.plots = values["-PLOT-"]
+            if event == "Start":
+                categories = ["square", "circle", "triangle", "star"]
+                paint_path = "C:/Users/chleb/Desktop/neural_network-master/paint_data"
+                img_size = 32
+                train_data = create_data(categories, img_size, paint_path)
+                td, _ = create_data_with_labels(train_data, img_size)
+                td = td / 255.0
+                print(td.shape)
+                print(td[0].shape)
                 break
-
-
 
         window.close()
